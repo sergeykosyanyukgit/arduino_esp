51e5c1d26069e64dfb9e29185cfec55caefef2b0
SergeyKosyanyuk <ser.ser.05021997@gmail.com>
2019-03-10 20:12:39 +0300

    modifed

--- EspPost/EspPost.ino
+++ EspPost/EspPost.ino
@@ -21,34 +21,20 @@ void setup() {
 }
  
 void loop() {
- 
  if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
- 
    HTTPClient http;
-   http.begin("https://mongo-express-vue-node.herokuapp.com/api/posts/");  //Specify destination for HTTP request
+   http.begin("https://mongo-express-vue-node.herokuapp.com/api/posts/esp/");  //Specify destination for HTTP request
    http.addHeader("Content-Type", "application/json");             //Specify content-type header
- 
-   int httpResponseCode = http.POST("{\"text\": \"Hello arduino day from DSTU\"}");   //Send the actual POST request
- 
+   int httpResponseCode = http.POST("{\"text\": \"Hello arduino day from DSTU\",\"type\": \"danger\"}");   //Send the actual POST request
    if(httpResponseCode>0){
- 
     Serial.println(httpResponseCode);   //Print return code
- 
    }else{
- 
     Serial.print("Error on sending request: ");
     Serial.println(httpResponseCode);
- 
    }
- 
    http.end();  //Free resources
- 
  }else{
- 
     Serial.println("Error in WiFi connection");   
- 
  }
- 
   delay(5000);  //Send a request every 5 seconds
- 
 }
